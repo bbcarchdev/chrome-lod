217e4126ddacfba7e70f3bc62ba9407244580e57
Alex Tucker <alex.tucker@bbc.co.uk>
2014-06-18 17:12:52 +0100

    Check for Turtle as well as XML/RDF.  Parse XML/RDF and look for licensing info.  Click on revealed RDF to make it full height/width.  Show traffic light icon depending on results of parsing.

--- background.js
+++ background.js
@@ -1,7 +1,25 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log("Message from content: " + request);
+    var messages = {
+      'error': 'Error parsing RDF',
+      'unlicensed': 'No machine readable licensing',
+      'licensed': 'License declared'
+    };
+    chrome.pageAction.setTitle({
+      tabId: sender.tab.id,
+      title: messages[request.check]
+    });
+    var icon = {
+      'error': 'red',
+      'unlicensed': 'amber',
+      'licensed': 'green'
+    };
     chrome.pageAction.show(sender.tab.id);
+    chrome.pageAction.setIcon({
+      tabId: sender.tab.id,
+      path: 'pageIcon-' + icon[request.check] + '.png'
+    });
     sendResponse({text: "ok"});
   }
 );
--- contentscript.js
+++ contentscript.js
@@ -1,50 +1,103 @@
-function peelBackRdf(data) {
+function peelBackRdf(data, format) {
   $('body').prepend('<div id="target"><textarea id="code"></textarea></div>');
   $('#code').text(data);
   var cm = CodeMirror.fromTextArea($('#code')[0], {
-    mode: "xml",
-    readOnly: "nocursor"      
+    mode: format,
+    readOnly: "nocursor",
+    viewportMargin: Infinity
   });
   $('#target').fold({
-    directory: "chrome-extension://" + chrome.runtime.id
+    directory: "chrome-extension://" + chrome.runtime.id,
+    autoCurl: true
   });
+  $('#turn_wrapper').click(
+    function() {
+      $('#turn_fold').hide();
+      $('#turn_wrapper').width('100%').height('100%').off();
+      $('#turn_object').width('100%').height('100%').off();
+      $('#turn_hideme').width('100%').height('100%').off();
+    }
+  );
+}
+
+function checkRdf(data, format) {
+  var parser = new DOMParser();
+  doc = parser.parseFromString(data, 'text/xml');
+  try {
+    var rdf = $.rdf().load(doc);
+    var licenses = rdf
+      .prefix('cc', 'http://creativecommons.org/ns#')
+      .where('?doc cc:license ?license');
+    licenses = licenses.add(
+      rdf
+        .prefix('dct', 'http://purl.org/dc/terms/')
+        .where('?doc dct:license ?license')
+    );
+    if (licenses.select().length > 0) {
+      return 'licensed';
+    } else {
+      return 'unlicensed';
+    }
+  } catch (e) {
+    console.log(e);
+    return 'error';
+  }
 }
 
 var res = document.evaluate("//link[(@rel = 'alternate') or (@rel = 'meta')]", document.head, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null)
 
 var rdfUrl = null;
+var rdfFormat = null;
+var acceptFormats = ['application/rdf+xml', 'text/turtle'];
 
 for (var i=0; i < res.snapshotLength; i++) {
-  if (res.snapshotItem(i).type == "application/rdf+xml") {
-    console.log("Found link rel=(alternate|meta) type=application/rdf+xml");
+  if (acceptFormats.indexOf(res.snapshotItem(i).type) > -1) {
+    rdfFormat = res.snapshotItem(i).type;
     rdfUrl = res.snapshotItem(i).href; // relative
-    chrome.runtime.sendMessage({type: 'rdf+xml'}, function(response) {
-      console.log(response.text);
-    });
     break;
   }
 }
 
 if (rdfUrl == null) { // Try fetching current page as RDF
   $.ajax(document.URL, {
-    accepts: { text: 'application/rdf+xml' },
+    headers: {
+      Accept: acceptFormats.join(', ')
+    },
     dataType: 'text',
     success: function(data, textStatus, res) {
       var ct = res.getResponseHeader('Content-Type');
-      if ((ct != null) && (ct.slice(0, 'application/rdf+xml'.length) == 'application/rdf+xml')) {
-        chrome.runtime.sendMessage({type: 'rdf+xml'}, function(response) {
-          console.log(response.text);
-        });
-        peelBackRdf(data);
+      if (ct != null) {
+        var format = null;
+        for (var i = 0; i < acceptFormats.length; i++) {
+          if (ct.slice(0, acceptFormats[i].length) == acceptFormats[i]) {
+            format = acceptFormats[i];
+            break;
+          }
+        }
+        if (format != null) {
+          peelBackRdf(data, format);
+          chrome.runtime.sendMessage({
+            type: format,
+	    check: checkRdf(data, format)
+          }, function(response) {
+	    console.log(response.text);
+          });
+        }
       }
     }
   });
 } else {
   $.ajax(rdfUrl, {
-    accepts: { text: 'application/rdf+xml' },
+    accepts: { text: rdfFormat },
     dataType: 'text',
     success: function(data, textStatus, res) {
-      peelBackRdf(data);
+      peelBackRdf(data, rdfFormat);
+      chrome.runtime.sendMessage({
+        type: format,
+        check: checkRdf(data, rdfFormat)
+      }, function(response) {
+        console.log(response.text);
+      });
     }
   });
 }
